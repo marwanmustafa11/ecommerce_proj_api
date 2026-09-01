815a94d73ff7f7dfcd695c3c8005ddb273712a4b
mohamedahmedbastawisi <mohamedahmedbastawisi@gmail.com>
2026-09-01 03:15:30 +0300

    Add OTP and Wishlist models

---
+++ models/OTP.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    otp: {
+        type: String,
+        required: true
+    },
+    expiresAt: {
+        type: Date,
+        required: true
+    },
+    userData: {
+        type: Object
+    }
+});
+
+module.exports = mongoose.model("OTP", otpSchema);
---
+++ models/Wishlist.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const wishlistSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+            index: true
+        },
+        products: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            }
+        ]
+    },
+    { timestamps: true }
+);
+
+
+wishlistSchema.pre(/^find/, function (next) {
+    this.populate("products");
+    next();
+});
+
+module.exports = mongoose.model("Wishlist", wishlistSchema);
