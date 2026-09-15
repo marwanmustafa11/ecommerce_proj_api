949cfe1dea0e19174b798d5d26b564b055deaef3
بشمهندس مروان <mustafamarwan752@gmail.com>
2026-09-15 17:32:14 +0300

    Update searchProducts.validation.js

--- validation/searchProducts.validation.js
+++ validation/searchProducts.validation.js
@@ -6,8 +6,11 @@ const searchProductsSchema = Joi.object({
   subcategory: Joi.string().optional(),
   brand: Joi.string().optional(),
   tags: Joi.string().optional(),
+
   minPrice: Joi.number().min(0).optional(),
   maxPrice: Joi.number().min(0).optional(),
+
+  rating: Joi.number().min(0).max(5).optional(),
 });
 
 export { searchProductsSchema };
